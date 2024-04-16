e3a448de1e0f4324c4a7e594ccfee4dff2ab5215
Daniel Martínez <150013573+DanielMartinezMartinez@users.noreply.github.com>
2024-04-16 13:24:53 +0200

    Add files via upload

--- ExamenJUNIT/src/es/iesmz/tests/Money.java
+++ ExamenJUNIT/src/es/iesmz/tests/Money.java
@@ -2,6 +2,9 @@ package es.iesmz.tests;
 
 public class Money {
     static final float cambioEURtoUSD = (float) 1.18798;
+    static final float cambioUSDtoEUR = (float) 0.841815;
+    static final float cambioEURtoGBP = (float) 0.857839;
+    static final float cambioGBPtoEUR = (float) 1.165826;
 
     static float change(TipoMoneda origen, TipoMoneda destino, float money){
         if (money < 0){
@@ -12,15 +15,50 @@ public class Money {
             EURtoUSD(money);
         }
 
-        if (origen == TipoMoneda.EUR && destino == TipoMoneda.USD){
-
+        if (origen == TipoMoneda.USD && destino == TipoMoneda.EUR){
+            USDtoEUR(money);
         }
 
+        if (origen == TipoMoneda.EUR && destino == TipoMoneda.GBP){
+            EURtoGBP(money);
+        }
+
+        if (origen == TipoMoneda.GBP && destino == TipoMoneda.EUR){
+            GBPtoEUR(money);
+        }
+
+        if (origen == TipoMoneda.USD && destino == TipoMoneda.GBP){
+            USDtoGBP(money);
+        }
+
+        if (origen == TipoMoneda.GBP && destino == TipoMoneda.USD){
+            GBPtoUSD(money);
+        }
+        return 0;
     }
 
     static float EURtoUSD(float cantidad) {
         return (float) (cantidad * cambioEURtoUSD);
     }
 
+    static float USDtoEUR(float cantidad) {
+        return (float) (cantidad * cambioUSDtoEUR);
+    }
+
+    static float EURtoGBP(float cantidad) {
+        return (float) (cantidad * cambioEURtoGBP);
+    }
+
+    static float GBPtoEUR(float cantidad) {
+        return (float) (cantidad * cambioGBPtoEUR);
+    }
+
+    static float USDtoGBP(float cantidad) {
+        return EURtoGBP(USDtoEUR(cantidad));
+    }
+
+    static float GBPtoUSD(float cantidad) {
+        return GBPtoEUR(EURtoUSD(cantidad));
+    }
 
 }
--- ExamenJUNIT/src/es/iesmz/tests/MoneyTest.java
+++ ExamenJUNIT/src/es/iesmz/tests/MoneyTest.java
@@ -10,62 +10,62 @@ class MoneyTest {
     public void test1(){
         float resultadoEsperado = Money.change(TipoMoneda.EUR,TipoMoneda.USD, 23.88f);
         float resultadoReal = 28.37f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
     @Test
     public void test2(){
         float resultadoEsperado = Money.change(TipoMoneda.GBP,TipoMoneda.EUR, 1000.0f);
         float resultadoReal = 1165.83f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
     @Test
     public void test3(){
         float resultadoEsperado = Money.change(TipoMoneda.EUR,TipoMoneda.GBP, 234.56f);
         float resultadoReal = 201.21f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
     @Test
     public void test4(){
         float resultadoEsperado = Money.change(TipoMoneda.USD,TipoMoneda.EUR, 44.56f);
         float resultadoReal = 37.51f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
     @Test
     public void test5(){
         float resultadoEsperado = Money.change(TipoMoneda.GBP,TipoMoneda.USD, 100.0f);
         float resultadoReal = 138.49f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
     @Test
     public void test6(){
         float resultadoEsperado = Money.change(TipoMoneda.USD,TipoMoneda.GBP, 1000.0f);
         float resultadoReal = 722.14f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
     @Test
     public void test7(){
         float resultadoEsperado = Money.change(TipoMoneda.PTS,TipoMoneda.EUR, 100.0f);
         float resultadoReal = -1f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
     @Test
     public void test8(){
         float resultadoEsperado = Money.change(TipoMoneda.EUR,TipoMoneda.PTS, 123.3f);
         float resultadoReal = -1f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
     @Test
     public void test9(){
         float resultadoEsperado = Money.change(TipoMoneda.USD,TipoMoneda.PTS, -167.34f);
         float resultadoReal = -1f;
-        assertEquals(resultadoEsperado,resultadoReal);
+        assertEquals(resultadoReal, resultadoEsperado);
     }
 
 }
